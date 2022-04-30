197da01d9937294a0da12a54af9501f5fdab10f6
PREVIRED\cfarias <cfarias@previred.com>
2022-04-30 17:46:47 -0400

    first commit

---
+++ src/test/java/cl/crisan/patrones/PatronesApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.crisan.patrones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
