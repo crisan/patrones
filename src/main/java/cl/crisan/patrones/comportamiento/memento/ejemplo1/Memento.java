7f56218515e4c23630d6605828a630129f6c738f
PREVIRED\cfarias <cfarias@previred.com>
2022-05-01 22:36:41 -0400

    Patron Memento

---
+++ src/main/java/cl/crisan/patrones/comportamiento/memento/ejemplo1/Memento.java
@@ -0,0 +1,14 @@
+package cl.crisan.patrones.comportamiento.memento.ejemplo1;
+
+public class Memento {
+
+    private String state;
+
+    public Memento(String state) {
+        this.state = state;
+    }
+
+    public String getState() {
+        return state;
+    }
+}
